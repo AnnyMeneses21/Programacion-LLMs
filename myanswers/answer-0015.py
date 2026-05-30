fc73df7b4550c6e957e87b83149d0ef305ff2bb8
AnnyMeneses21 <anny.meneses@udea.edu.co>
2026-05-29 19:47:45 -0500

    Update answer-0015.py

--- myanswers/answer-0015.py
+++ myanswers/answer-0015.py
@@ -6,13 +6,13 @@ from sklearn.preprocessing import RobustScaler
 def preparar_datos(df, target_col):
     X = df.drop(columns=[target_col])
 
-    # ndarray de strings, pero forzado a dtype=str
-    y = np.array(df[target_col].astype(str).tolist(), dtype=str)
-
     imputer = SimpleImputer(strategy='mean')
     X_imputed = imputer.fit_transform(X)
 
     scaler = RobustScaler()
     X_scaled = scaler.fit_transform(X_imputed)
+
+    # devolver lista en lugar de ndarray
+    y = list(df[target_col])
 
     return X_scaled, y
