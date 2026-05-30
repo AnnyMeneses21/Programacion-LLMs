b402601a49e1a97d4d2287fbd28c2f67af1806b9
AnnyMeneses21 <anny.meneses@udea.edu.co>
2026-05-30 08:36:17 -0500

    Update answer-0015.py

--- myanswers/answer-0015.py
+++ myanswers/answer-0015.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler
+
 def preparar_datos(df, target_col):
     X = df.drop(columns=[target_col])
     y = df[target_col].values
