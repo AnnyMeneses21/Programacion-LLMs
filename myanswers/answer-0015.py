a519e47ee142ff9b1f402c6adb97fdec74471cdd
AnnyMeneses21 <anny.meneses@udea.edu.co>
2026-05-29 20:05:25 -0500

    Update answer-0015.py

--- myanswers/answer-0015.py
+++ myanswers/answer-0015.py
@@ -1,18 +1,16 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler
 
 def preparar_datos(df, target_col):
     X = df.drop(columns=[target_col])
+    y = df[target_col].values
 
     imputer = SimpleImputer(strategy='mean')
     X_imputed = imputer.fit_transform(X)
 
     scaler = RobustScaler()
     X_scaled = scaler.fit_transform(X_imputed)
-
-    # versión de prueba
-    y = pd.Series(df[target_col].values)
 
     return X_scaled, y
