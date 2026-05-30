5e9df891691523e8c44148f7bb96ac5b0f27659f
AnnyMeneses21 <anny.meneses@udea.edu.co>
2026-05-29 19:26:34 -0500

    Update answer-0015.py

--- myanswers/answer-0015.py
+++ myanswers/answer-0015.py
@@ -1,6 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler
+
 def preparar_datos(df, target_col):
     X = df.drop(columns=[target_col])
-    y = df[target_col].values
+
+    # ndarray de strings, pero forzado a dtype=str
+    y = np.array(df[target_col].astype(str).tolist(), dtype=str)
 
     imputer = SimpleImputer(strategy='mean')
     X_imputed = imputer.fit_transform(X)
