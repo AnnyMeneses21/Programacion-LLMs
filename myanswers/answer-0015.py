d3d38dc3c48dfd81466f0c4c56c89d05ff595104
AnnyMeneses21 <anny.meneses@udea.edu.co>
2026-05-30 13:24:56 -0500

    Update answer-0015.py

--- myanswers/answer-0015.py
+++ myanswers/answer-0015.py
@@ -3,14 +3,11 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler
 
-def preparar_datos(df, target_col):
+def masas_agua(df, target_col):
     X = df.drop(columns=[target_col])
-    y = df[target_col]
-
+    y = df[target_col].values  # <-- .values aquí
     imputer = SimpleImputer(strategy='mean')
     X_imputed = imputer.fit_transform(X)
-
     scaler = RobustScaler()
     X_scaled = scaler.fit_transform(X_imputed)
-
     return X_scaled, y
